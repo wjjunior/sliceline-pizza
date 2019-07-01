03319e38aee690189184af7f45055d91e9d44645
Wagner Junior <wagner.junior@dito.com.br>
2019-07-01 17:36:38 -0300

    Add Price

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,43 +1,58 @@
+export function formatPrice(price) {
+    return price.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+}
+
 export const foodItems = [
     {
         name: 'Cheese Pizza',
         img: '/img/pizza.png',
-        section: 'Pizza'
+        section: 'Pizza',
+        price: 1
     },
     {
         name: 'Pepperoni Pizza',
         img: '/img/pizza2.jpeg',
-        section: 'Pizza'
+        section: 'Pizza',
+        price: 1.5
     },
     {
         name: 'Chicken Pizza',
         img: '/img/chicken-pizza.jpeg',
-        section: 'Pizza'
+        section: 'Pizza',
+        price: 2
     },
     {
         name: 'Veggie Pizza',
         img: '/img/healthy-pizza.jpeg',
-        section: 'Pizza'
+        section: 'Pizza',
+        price: 2
     },
     {
         name: 'Burger',
         img: '/img/burger.jpeg',
-        section: 'Sandwich'
+        section: 'Sandwich',
+        price: 3
     },
     {
         name: 'Gyro',
         img: '/img/gyro.jpeg',
-        section: 'Sandwich'
+        section: 'Sandwich',
+        price: 4.5
     },
     {
         name: 'Shrimp PoBoy',
         img: '/img/sandwich.jpeg',
-        section: 'Sandwich'
+        section: 'Sandwich',
+        price: 6
     },
     {
         name: 'Fries',
         img: '/img/fries.jpeg',
-        section: 'Sides'
+        section: 'Sides',
+        price: 1
     }
 ]
 
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { foods } from '../Data/FoodData'
 import { Food, FoodGrid, FoodLabel } from './FoodGrid'
+import { formatPrice } from '../Data/FoodData'
 
 const MenuStyled = styled.div`
     height: 1000px;
@@ -19,7 +20,8 @@ export function Menu({ setOpenFood }) {
                             setOpenFood(food)
                         }}>
                             <FoodLabel>
-                                {food.name}
+                                <div>{food.name}</div>
+                                <div>{formatPrice(food.price)}</div>
                             </FoodLabel>
                         </Food>
                     ))}
