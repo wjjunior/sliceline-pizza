e318d2d4cc71de869ef0e20d2a26016ae5b48006
Wagner Junior <wagner.junior@dito.com.br>
2019-07-01 16:04:14 -0300

    Add Order and FoodDialogFooter

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { Navbar } from "./Navbar/Navbar"
 import { Banner } from "./Banner/Banner"
 import { Menu } from './Menu/Menu'
 import { FoodDialog } from './FoodDialog/FoodDialog'
+import { Order } from './Order/Order'
 
 function App() {
 
   const [openFood, setOpenFood] = useState()
-  
+
   return (
     <>
       <GlobalStyle />
-      <FoodDialog openFood={openFood} setOpenFood={setOpenFood}/>
+      <FoodDialog openFood={openFood} setOpenFood={setOpenFood} />
       <Navbar />
+      <Order />
       <Banner />
       <Menu setOpenFood={setOpenFood} />
     </>
