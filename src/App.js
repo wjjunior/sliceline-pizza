5738a05b4a093b5c1936bee2413e9577cb0e85c1
Wagner Junior <wagner.junior@dito.com.br>
2019-07-01 13:11:04 -0300

    Add MEnu, FoodData and FoodGrid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components'
 import { Navbar } from "./Navbar/Navbar"
 import { Banner } from "./Banner/Banner"
+import { Menu } from './Menu/Menu'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -19,6 +20,7 @@ function App() {
       <GlobalStyle />
       <Navbar/>
       <Banner/>
+      <Menu/>
       <div>Hello Wagner</div>
     </>
   );
