c22a5739068f779cb9babd24e81cb3b7c8dd7aa5
Wagner Junior <wagner.junior@dito.com.br>
2019-07-01 13:11:31 -0300

    Food images

---
+++ src/Data/FoodData.js
@@ -0,0 +1,34 @@
+export const foods = [
+    {
+        name: 'Cheese Pizza',
+        img: '/img/pizza.png'
+    },
+    {
+        name: 'Pepperoni Pizza',
+        img: '/img/pizza2.jpeg'
+    },
+    {
+        name: 'Chicken Pizza',
+        img: '/img/chicken-pizza.jpeg'
+    },
+    {
+        name: 'Veggie Pizza',
+        img: '/img/healthy-pizza.jpeg'
+    },
+    {
+        name: 'Burger',
+        img: '/img/burger.jpeg'
+    },
+    {
+        name: 'Gyro',
+        img: '/img/gyro.jpeg'
+    },
+    {
+        name: 'Shrimp PoBoy',
+        img: '/img/sandwich.jpeg'
+    },
+    {
+        name: 'Fries',
+        img: '/img/fries.jpeg'
+    }
+]
