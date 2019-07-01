8afc20ead47e5a851fa062fe27f7705b0fdb7db4
Wagner Junior <wagner.junior@dito.com.br>
2019-07-01 20:50:32 -0300

    Add Toppings

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -6,6 +6,8 @@ import { Title } from '../Styles/title'
 import { formatPrice } from '../Data/FoodData'
 import { QuantityInput } from './QuantityInput'
 import { useQuantity } from '../Hooks/useQuantity'
+import { Toppings } from './Toppings'
+import { useToppings } from '../Hooks/useToppings'
 
 const Dialog = styled.div`
     width: 500px;
@@ -23,6 +25,7 @@ export const DialogContent = styled.div`
     overflow: auto;
     min-height: 100px;
     padding: 0px 40px;
+    padding-bottom: 80px;
 `
 export const ConfirmButton = styled(Title)`
     margin: 10px;
@@ -66,20 +69,27 @@ const DialogBannerName = styled(FoodLabel)`
     font-size: 30px;
     padding: 5px 40px;
 `
+const pricePerTopping = 0.5
 
 export function getPrice(order) {
-    return order.quantity * order.price
+    return order.quantity * (order.price + order.toppings.filter(t => t.checked).length * pricePerTopping)
+}
+
+function hasToppings(food) {
+    return food.section === 'Pizza'
 }
 
 function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
     const quantity = useQuantity(openFood && openFood.quantity)
+    const toppings = useToppings(openFood.toppings)
     function close() {
         setOpenFood()
     }
 
     const order = {
         ...openFood,
-        quantity: quantity.value
+        quantity: quantity.value,
+        toppings: toppings.toppings
     }
 
     function addToOrder() {
@@ -97,6 +107,10 @@ function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
                     </DialogBanner>
                     <DialogContent>
                         <QuantityInput quantity={quantity} />
+                        {hasToppings(openFood) && <>
+                            <h3> Would you like toppings? </h3>
+                            <Toppings {...toppings} />
+                        </>}
                     </DialogContent>
                     <DialogFooter>
                         <ConfirmButton onClick={addToOrder}>
--- src/Order/Order.js
+++ src/Order/Order.js
@@ -34,6 +34,11 @@ const OrderItem = styled.div`
     justify-content: space-between;
 `
 
+const DetailItem = styled.div`
+    color: gray;
+    font-size: 10px;
+`
+
 export function Order({ orders }) {
     const subtotal = orders.reduce((total, order) => {
         return total + getPrice(order)
@@ -55,6 +60,14 @@ export function Order({ orders }) {
                                 <div />
                                 <div>{formatPrice(getPrice(order))}</div>
                             </OrderItem>
+                            <DetailItem>
+                                {
+                                    order.toppings
+                                        .filter(t => t.checked)
+                                        .map(topping => topping.name)
+                                        .join(", ")
+                                }
+                            </DetailItem>
                         </OrderContainer>
                     ))}
                     <OrderContainer>
