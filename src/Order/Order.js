e75c0826e51ae9d8c79f48c14644bc086e6d8202
Wagner Junior <wagner.junior@dito.com.br>
2019-07-01 19:57:16 -0300

    Add total, tax and subtotal

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -35,6 +35,11 @@ const OrderItem = styled.div`
 `
 
 export function Order({ orders }) {
+    const subtotal = orders.reduce((total, order) => {
+        return total + getPrice(order)
+    }, 0)
+    const tax = subtotal * 0.07
+    const total = subtotal + tax;
     return <OrderStyled>
         {orders.length === 0 ? (
             <OrderContent>  Your order's looking pretty empty. </OrderContent>
@@ -52,6 +57,23 @@ export function Order({ orders }) {
                             </OrderItem>
                         </OrderContainer>
                     ))}
+                    <OrderContainer>
+                        <OrderItem>
+                            <div />
+                            <div>Sub-Total</div>
+                            <div>{formatPrice(subtotal)}</div>
+                        </OrderItem>
+                        <OrderItem>
+                            <div />
+                            <div>Tax</div>
+                            <div>{formatPrice(tax)}</div>
+                        </OrderItem>
+                        <OrderItem>
+                            <div />
+                            <div>Total</div>
+                            <div>{formatPrice(total)}</div>
+                        </OrderItem>
+                    </OrderContainer>
                 </OrderContent>
             )}
         <DialogFooter>
